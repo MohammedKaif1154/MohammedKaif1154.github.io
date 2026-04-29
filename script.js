8aafd504438cb72f3838acd2ae1e245f5e0c108c
Mohammed Kaif Faiyaz Shaikh <s.kaifali786@gmail.com>
2026-04-29 13:29:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-alert("Your portfolio is working!");
+const text = "I build machine learning solutions using Python, SQL, and Azure.";
+let i = 0;
+
+function typeEffect() {
+  if (i < text.length) {
+    document.getElementById("typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeEffect, 30);
+  }
+}
+
+typeEffect();
