0820eed478a644e8d50276aac2ff99f9a3ac5d00
Mohammed Kaif Faiyaz Shaikh <s.kaifali786@gmail.com>
2026-04-29 14:56:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
-const text = "I build machine learning solutions using Python, SQL, and Azure.";
-let i = 0;
+document.addEventListener("DOMContentLoaded", () => {
 
-function typeEffect() {
-  if (i < text.length) {
-    document.getElementById("typing").innerHTML += text.charAt(i);
-    i++;
-    setTimeout(typeEffect, 30);
+  const text = "I build machine learning solutions using Python, SQL, and Azure.";
+  let i = 0;
+
+  function typeEffect() {
+    if (i < text.length) {
+      document.getElementById("typing").innerHTML += text.charAt(i);
+      i++;
+      setTimeout(typeEffect, 30);
+    }
   }
-}
 
-typeEffect();
+  typeEffect();
+
+  const elements = document.querySelectorAll(".fade");
+
+  function showOnScroll() {
+    elements.forEach(el => {
+      const position = el.getBoundingClientRect().top;
+
+      if (position < window.innerHeight - 100) {
+        el.classList.add("show");
+      }
+    });
+  }
+
+  // Run once on load
+  showOnScroll();
+
+  // Run on scroll
+  window.addEventListener("scroll", showOnScroll);
+
+});
